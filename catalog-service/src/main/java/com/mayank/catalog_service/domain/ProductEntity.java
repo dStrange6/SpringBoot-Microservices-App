fa98729b2b9fba8ef1cf14fa6cee23f691be5e2c
dStrange6 <pathakmayank021@gmail.com>
2026-01-17 14:58:24 +0530

    controller, service and repo added

---
+++ catalog-service/src/main/java/com/mayank/catalog_service/domain/ProductEntity.java
@@ -0,0 +1,37 @@
+package com.mayank.catalog_service.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class ProductEntity
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_id_generator")
+    @SequenceGenerator(name = "product_id_generator", sequenceName = "product_id_seq")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @NotEmpty(message = "Product code is required")
+    private String code;
+
+    @Column(nullable = false)
+    @NotEmpty(message = "Product name is required")
+    private String name;
+
+    private String description;
+    private String imageUrl;
+
+    @Column(nullable = false)
+    @NotNull(message = "Product price is required") @DecimalMin("0.1")
+    private BigDecimal price;
+}
---
+++ catalog-service/src/main/java/com/mayank/catalog_service/web/controllers/ProductController.java
@@ -0,0 +1,29 @@
+package com.mayank.catalog_service.web.controllers;
+
+import com.mayank.catalog_service.domain.PagedResult;
+import com.mayank.catalog_service.domain.Product;
+import com.mayank.catalog_service.domain.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController
+{
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    PagedResult<Product> getProducts(@RequestParam(name="page", defaultValue = "1")int pageNo)
+    {
+        return productService.getProducts(pageNo);
+    }
+
+}
